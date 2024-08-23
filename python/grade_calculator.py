9c6aa71084f86850f52a3708f067d5817b38ac6f
Fritz G. Batroni <fritz.batroni@celeritycommerce.com>
2024-08-23 18:17:59 -0400

    adding python folder for hands-on labs

---
+++ python/grade_calculator.py
@@ -0,0 +1,8 @@
+"""
+A Python program to calculate and format grades.
+Author: [Your Name]
+Course: Software Engineering Bootcamp
+Date: [Date of last modification]
+Description: This program calculates the average and final letter grades based on user-inputted grades.
+"""
+
